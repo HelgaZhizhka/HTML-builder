1f588a1ab993967f74fd991c84bb17aac0248ce7
HelgaZhizhka <helgazhyzhka@gmail.com>
2023-05-04 17:39:41 +0200

    feat: implement task 4

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,37 @@
+const fsp = require('fs/promises');
+const path = require('path');
+
+async function copyDir(src, dest) {
+  try {
+    await fsp.rm(destDir, { recursive: true, force: true });
+  } catch (err) {
+    console.error(`Ошибка удаления содержимого папки: ${err.message}`);
+  }
+
+  try {
+    await fsp.mkdir(dest, { recursive: true });
+  } catch (err) {
+    console.error(`Ошибка создания папки: ${err.message}`);
+  }
+
+  const files = await fsp.readdir(src, {withFileTypes: true});
+
+  for (const file of files) {
+    const srcPath = path.join(src, file.name);
+    const destPath = path.join(dest, file.name);
+    if (file.isFile()) {
+      await fsp.copyFile(srcPath, destPath);
+    } else {
+      try {
+        await copyDir(srcPath, destPath);
+      } catch (err) {
+        console.error('Ошибка при копировании папки:', err);
+      }
+    }
+  }
+}
+
+const srcDir = path.join(__dirname, 'files');
+const destDir = path.join(__dirname, 'files-copy');
+copyDir(srcDir, destDir).then(() => console.log('Копирование папки прошло успешно'));
+
